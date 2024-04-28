cd67faf7c1b4f690c043d567c772812abde422c0
Yujie <yp25@st-andrews.ac.uk>
2024-04-28 13:26:37 +0100

    finish header with animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,16 @@
 import { motion } from "framer-motion"
 import { links } from "@/lib/data"
 import Link from "next/link"
+import { useState } from "react"
+import clsx from "clsx"
 
 function Header() {
+  const [activeSection, setActiveSection] = useState("Home")
   return (
     <header className="z-[999] relative">
       <motion.div
         className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-40
-        shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -18,15 +21,26 @@ function Header() {
           {links.map((link, index) => (
             <motion.li
               key={link.hash}
-              className="h-3/4 flex items-center justify-center"
+              className="h-3/4 flex items-center justify-center relative"
               initial={{ opacity: 0, y: -100 }}
               animate={{ opacity: 1, y: 0 }}
             >
               <Link
                 href={link.hash}
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
+                  { "text-gray-950": activeSection === link.name }
+                )}
+                onClick={() => setActiveSection(link.name)}
               >
                 {link.name}
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                    layoutId="activeSection"
+                    transition={{ type: "spring", stiffness: 380, damping: 30 }}
+                  ></motion.span>
+                )}
               </Link>
             </motion.li>
           ))}
