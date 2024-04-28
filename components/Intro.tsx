dc9f331b2e43270daf5c6e27306019ec95e49604
Yujie <yp25@st-andrews.ac.uk>
2024-04-28 12:38:15 +0100

    add hash achor link and improve padding by scroll-mt-[value]

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -9,7 +9,10 @@ import Link from "next/link"
 
 export function Intro() {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-28"
+      id="home"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,7 @@ import Project from "./Project"
 
 export function Projects() {
   return (
-    <section>
+    <section id="projects" className="scroll-mt-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
