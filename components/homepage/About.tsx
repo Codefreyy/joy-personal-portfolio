3d6367bee9df1536e67b7cc44e1096f527eb1f9f
Yujie <yp25@st-andrews.ac.uk>
2024-05-08 00:28:39 +0100

    update about me section

--- components/homepage/About.tsx
+++ components/homepage/About.tsx
@@ -38,38 +38,37 @@ export default function About() {
                 Wuhan University
               </a>
             </span>
-            , I found a deep passion for programming. This journey began with
-            classes in the computer science fundamentals, databases and
-            HTML/CSS, which led me directly to{" "}
+            , I found a strong interest in programming. The journey started with
+            basic courses in computer science, databases and HTML/CSS, which
+            naturally led me to{" "}
             <span className="font-medium italic">front-end development</span>.
           </p>
 
           <p className="mb-3">
-            {" "}
-            After interning at the Big Data Research Institute of Wuhan
-            University and{" "}
+            Internships at Wuhan University&apos;s Big Data Institute and{" "}
             <span className="italic underline">
               <a href="https://www.nio.com/careers" target="_blank">
                 NIO Inc.
               </a>
-            </span>
-            , I became even more certain that this was the career path I wanted
-            to pursue. Currently, I am pursuing a Master’s degree in
-            <span className="font-medium italic"> Computing and IT</span> at the{" "}
+            </span>{" "}
+            confirmed that this is the career path I want to pursue. I am
+            currently studying for an
+            <span className="font-medium italic">
+            {" "}  MSc in Computing and IT
+            </span>{" "}
+            at the{" "}
             <span className="italic underline">
               <a href="https://www.st-andrews.ac.uk/" target="_blank">
-                University of St Andrews
+                University of St Andrews.
               </a>
             </span>
           </p>
 
           <p className="mb-3">
-            I enjoy the challenge of programming, and the process of working
-            with a team to develop projects and solve complex problems brings
-            great satisfaction. I specialize in working with technologies such
-            as{" "}
-            <span className="font-medium italic">React, Vue3, and Next.js</span>
-            , and have a solid understanding of{" "}
+            I thrive on programming challenges and enjoy working with teams to
+            solve complex problems. I specialize in technologies such as
+            <span className="font-medium italic"> React, Next.js and Vue</span>,
+            and have a solid understanding of{" "}
             <span className="font-medium italic">
               JavaScript, TypeScript and HTML/CSS
             </span>
@@ -80,9 +79,12 @@ export default function About() {
             In my spare time, I like to learn new skills and work on interesting
             projects 🛠️. I also run my own accounts on social media like
             Bilibili and Xiaohongshu to share technical knowledge and learning
-            resources 🌐. If I&apos;m not in front of the computer, I like to
-            cook, watch movies and work out 🍳🎥💪 Regular exercise, sleep and
-            diet are the secret to my energy 🌟.
+            resources. If I&apos;m not at the computer, I enjoy
+            <span className="font-medium italic">
+              {" "}
+              cooking, watching movies and working out.
+            </span>
+            Regular exercise, sleep and diet are my secrets to my energy.
           </p>
         </>
       )}
