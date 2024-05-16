79872fcf91eea9d034bb90b0600f5c4c0d9ab8ac
Yujie <yp25@st-andrews.ac.uk>
2024-05-16 19:01:07 +0100

    fix build error

--- components/ExperienceLabel.tsx
+++ components/ExperienceLabel.tsx
@@ -1,7 +1,7 @@
 import { useSectionInView } from "@/lib/hooks"
 
 export function ExperienceLabel() {
-  const { ref } = useSectionInView("Experience", 0.1)
+  const { ref } = useSectionInView("Experiences", 0.1)
   return (
     <section
       className="absolute top-96  mt-28 scroll-mt-[600px] w-10 h-[300px] bg-transparent"
