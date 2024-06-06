62bf38212244144e4036726a8069b0cadc91130b
Yujie <yp25@st-andrews.ac.uk>
2024-06-06 18:09:45 +0100

    download chinese resume in chinese language mode and update avatar

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -137,7 +137,9 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link> */}
         <a
-          href="/Joy-fullstack.pdf"
+          href={
+            activeLocale == "en" ? "/Joy-fullstack.pdf" : "/前端开发-彭郁洁.pdf"
+          }
           download={true}
           className="group text-sm  px-4 py-2  bg-white sm:text-lg sm:px-7 sm:py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
         >
